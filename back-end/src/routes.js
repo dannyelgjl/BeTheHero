31340ba84f272753cefc39f2bc571cfc923f9a9b
dannyelgjl <dannyelgjl@gmail.com>
2020-03-24 18:47:13 -0300

    get->profilercontroller

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -2,11 +2,14 @@ import { Router } from 'express';
 
 import OngController from './app/controllers/OngController';
 import IncidentsController from './app/controllers/IncidentController';
+import ProfileController from './app/controllers/ProfileController';
 
 const routes = new Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.store);
+
+routes.get('/profile', ProfileController.index);
 
 routes.post('/incidents', IncidentsController.store);
 routes.get('/incidents', IncidentsController.index);
